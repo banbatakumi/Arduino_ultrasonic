0fe616bf9d6f4a3e46bd2cf9dd95a484af4e0dfe
banbatakumi <bantech0210@gmail.com>
2024-02-19 19:59:50 +0900

    コメ追加

--- lib/ir/ir.cpp
+++ lib/ir/ir.cpp
@@ -12,13 +12,14 @@ Ir::Ir(uint8_t ir_0_, uint8_t ir_1_, uint8_t ir_2_, uint8_t ir_3_) {
       for (uint8_t i = 0; i < 4; i++) {
             pinMode(ir[i], INPUT);
       }
-      for (uint8_t i = 0; i < IR_QTY; i++) {
-            unit_vector_x[i] = cos((i * 360.00000 / IR_QTY + 45) * PI / 180.00000);
-            unit_vector_y[i] = sin((i * 360.00000 / IR_QTY + 45) * PI / 180.00000);
+      for (uint8_t i = 0; i < IR_QTY; i++) {  // 各センサに座標を与える
+            unit_vector_x[i] = cos((i * 360.00000 / IR_QTY + 45) * PI / 180.0f);
+            unit_vector_y[i] = sin((i * 360.00000 / IR_QTY + 45) * PI / 180.0f);
       }
 }
 
 void Ir::Read() {
+      // センサそれぞれの値を読む
       for (uint8_t i = 0; i < IR_QTY; i++) val[i] = 0;
       for (uint16_t i = 0; i < READ_NUM_OF_TIME; i++) {
             val[0] += PINB & 0b00000001;         // DigitalRead(ir[0]);
@@ -30,14 +31,16 @@ void Ir::Read() {
       result_vector_x = 0;
       result_vector_y = 0;
       for (uint8_t i = 0; i < IR_QTY; i++) {
-            val[i] = (READ_NUM_OF_TIME - val[i]) * (100.00000 / READ_NUM_OF_TIME);
-            result_vector_x += val[i] * unit_vector_x[i];
-            result_vector_y += val[i] * unit_vector_y[i];
+            val[i] = (READ_NUM_OF_TIME - val[i]) * (100.0f / READ_NUM_OF_TIME);  // 100までに補正
+            result_vector_x += val[i] * unit_vector_x[i];                        // X成分を合成
+            result_vector_y += val[i] * unit_vector_y[i];                        // Y成分を合成
       }
-      dir = atan2(result_vector_y, result_vector_x) / PI * 180.000000;
-      dis = sqrt(pow(result_vector_x, 2) + pow(result_vector_y, 2)) * 1.8;
+
+      dir = atan2(result_vector_y, result_vector_x) / PI * 180.0f;          // 角度を出す
+      dis = sqrt(pow(result_vector_x, 2) + pow(result_vector_y, 2)) * 1.8;  // 距離を出す
       if (dis > 100) dis = 100;
 
+      // 移動平均を取る
       DirAve.Compute(&dir);
       DisAve.Compute(&dis);
 }
